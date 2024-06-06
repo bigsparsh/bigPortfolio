ad33bf3f48476ee7830063fca46cf0431d79be3b
Sparsh Singh <sparshsingh7586@gmail.com>
2024-06-06 09:52:53 +0530

    Added POST request for adding projects

---
+++ app/api/project/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getProjects } from "@/app/actions/projects";
+import db from "@/db";
+import { ProjectTable } from "@/db/schema";
+
+export async function GET() {
+  return NextResponse.json({
+    message: "Here is the list of projects",
+    projects: await getProjects(),
+  });
+}
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  console.log(body);
+  const newProject = await db
+    .insert(ProjectTable)
+    .values(body)
+    .returning({ id: ProjectTable.id });
+  return NextResponse.json({
+    message: "Project created successfully",
+    project: newProject,
+  });
+}
