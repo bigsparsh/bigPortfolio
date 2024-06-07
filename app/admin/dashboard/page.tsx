22bc99dceb6ff0924b5b59b685bd8fcd27ee705d
Sparsh Singh <sparshsingh7586@gmail.com>
2024-06-07 17:24:53 +0530

    Added logic for admin dashboard

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { createProject } from "@/actions/projects";
-import { useRef } from "react";
+import Toast from "@/components/Toast";
+import { AnimatePresence } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 
 const Dashboard = () => {
   const name = useRef<HTMLInputElement>(null);
@@ -8,6 +10,18 @@ const Dashboard = () => {
   const githubUrl = useRef<HTMLInputElement>(null);
   const websiteUrl = useRef<HTMLInputElement>(null);
   const imageUrl = useRef<HTMLInputElement>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    window.onclick = () => {
+      setError(null);
+    };
+
+    return () => {
+      window.removeEventListener("click", () => { });
+    };
+  }, []);
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     const project = await createProject({
@@ -17,10 +31,22 @@ const Dashboard = () => {
       websiteUrl: websiteUrl.current?.value || "",
       imageUrl: imageUrl.current?.value || "",
     });
-    console.log(project);
+
+    (name.current as HTMLInputElement).value = "";
+    (description.current as HTMLInputElement).value = "";
+    (githubUrl.current as HTMLInputElement).value = "";
+    (websiteUrl.current as HTMLInputElement).value = "";
+    (imageUrl.current as HTMLInputElement).value = "";
+
+    if (!project) {
+      setError("There was an error creating the project");
+      return;
+    }
+    setError("Project created successfully");
   };
   return (
     <div className="h-screen w-screen flex justify-center items-center">
+      <AnimatePresence>{error && <Toast>{error}</Toast>}</AnimatePresence>
       <form
         className="w-[500px] h-fit p-10 rounded-3xl bg-primary-400/50 flex flex-col gap-4"
         onSubmit={handleSubmit}
@@ -29,31 +55,31 @@ const Dashboard = () => {
           type="text"
           placeholder="Enter the name of the project"
           ref={name}
-          className="rounded-full bg-primary-400 px-4 py-2 placeholder:text-white/50 text-white"
+          className="rounded-full bg-primary-400 px-4 py-2 placeholder:text-white/50 text-white outline-none"
         />
         <input
           type="text"
           placeholder="Enter information about the project"
           ref={description}
-          className="rounded-full bg-primary-400 px-4 py-2 placeholder:text-white/50 text-white"
+          className="rounded-full bg-primary-400 px-4 py-2 placeholder:text-white/50 text-white outline-none"
         />
         <input
           type="text"
           placeholder="Enter the website url if it is hosted somewhere"
           ref={websiteUrl}
-          className="rounded-full bg-primary-400 px-4 py-2 placeholder:text-white/50 text-white"
+          className="rounded-full bg-primary-400 px-4 py-2 placeholder:text-white/50 text-white outline-none"
         />
         <input
           type="text"
           placeholder="Enter the github url where the source code is"
           ref={githubUrl}
-          className="rounded-full bg-primary-400 px-4 py-2 placeholder:text-white/50 text-white"
+          className="rounded-full bg-primary-400 px-4 py-2 placeholder:text-white/50 text-white outline-none"
         />
         <input
           type="text"
           placeholder="Add a screenshot url"
           ref={imageUrl}
-          className="rounded-full bg-primary-400 px-4 py-2 placeholder:text-white/50 text-white"
+          className="rounded-full bg-primary-400 px-4 py-2 placeholder:text-white/50 text-white outline-none"
         />
         <button className="px-4 py-2 bg-primary-100 text-white rounded-full h-fit self-end">
           Add +{" "}
